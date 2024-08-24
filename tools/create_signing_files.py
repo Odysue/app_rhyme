41cb33f12e853c31850a9ae088067d8aef14b3cf
Odysue <99384317+Odysue@users.noreply.github.com>
2024-08-24 23:13:58 +0800

    Update create_signing_files.py

--- tools/create_signing_files.py
+++ tools/create_signing_files.py
@@ -5,6 +5,12 @@ import base64
 apk_sign_pwd = os.getenv('APK_SIGN_PWD')
 apk_sign_jks = os.getenv('APK_SIGN_JKS')
 apk_sign_alias = os.getenv('APK_SIGN_ALIAS')
+print(type(apk_sign_pwd))
+print(len(apk_sign_pwd))
+print(type(apk_sign_alias))
+print(len(apk_sign_pwd))
+print(type(apk_sign_jks))
+print(len(apk_sign_jks))
 
 
 
